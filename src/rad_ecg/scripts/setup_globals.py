6d0f781ec06432c5803a69f27c604ce97c85a024
Landcruiser87 <andyheroy@gmail.com>
2025-02-03 13:12:36 -0600

    refactor: removing old setup funcs
    removing old file choosing logic that
    was really confusing to begin with.
    much cleaner now.

--- src/rad_ecg/scripts/setup_globals.py
+++ src/rad_ecg/scripts/setup_globals.py
@@ -76,214 +76,112 @@ def authenticate_with_gcs(credentials_path:str):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
 
 ################################ Downloading Funcs ############################################
-#FUNCTION download individual ecg from gcs
-def download_ecg_from_gcs(bucket_name:str, save_path:str, logger:logging):
-    """Download function for GCS.  It will download all files within a bucket to the inputdata
-    folder for ingestion.  Note: Downloading via this method is slower, sometimes its easier
-    to copy the data over to the inputdata directory.
+# #FUNCTION download individual ecg from gcs
+# def download_ecg_from_gcs(bucket_name:str, save_path:str, logger:logging):
+#     """Download function for GCS.  It will download all files within a bucket to the inputdata
+#     folder for ingestion.  Note: Downloading via this method is slower, sometimes its easier
+#     to copy the data over to the inputdata directory.
 
-    Args:
-        bucket_name (str): GCS bucket target
-        save_path (str): full save path on VM
-        logger (logging): To log things of course
+#     Args:
+#         bucket_name (str): GCS bucket target
+#         save_path (str): full save path on VM
+#         logger (logging): To log things of course
 
-    Returns:
-        filenames (list): List of the header files found within the target data directory
-    """
+#     Returns:
+#         filenames (list): List of the header files found within the target data directory
+#     """
+#     #TODO - Save for eventaul refactor  
+#     #Might delete this function
+#     # Refactor this function to download 
+#     client = storage.Client()
+#     bucket = client.bucket(bucket_name)
 
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
+#     # Download all related files for the record
+#     blobs = list(bucket.list_blobs())
+#     gcp_folders = {}
+#     file_names = []
+#     unprocessed = []
 
-    # Download all related files for the record
-    blobs = list(bucket.list_blobs())
-    gcp_folders = {}
-    file_names = []
-    unprocessed = []
-
-    #Make a dict of the folders and their contents
-    for blob in blobs:
-        folder = os.path.dirname(blob.name)
-        if folder not in gcp_folders:
-            gcp_folders[folder] = []
-        if not blob.name.endswith("/"):
-            gcp_folders[folder].append(blob)
-        if blob.name.startswith("unprocessed"):
-            unprocessed.append(blob.name)
+#     #Make a dict of the folders and their contents
+#     for blob in blobs:
+#         folder = os.path.dirname(blob.name)
+#         if folder not in gcp_folders:
+#             gcp_folders[folder] = []
+#         if not blob.name.endswith("/"):
+#             gcp_folders[folder].append(blob)
+#         if blob.name.startswith("unprocessed"):
+#             unprocessed.append(blob.name)
     
-    #Generate list of all unprocessed cams
-    unprocessed = list(set([name.split("/")[1] for name in unprocessed]))
-    unprocessed = sorted([x for x in unprocessed if len(x) > 1])
-    logger.warning("please select a cam to download")
-    for idx, file in enumerate(unprocessed):
-        logger.warning(f"{idx} : {file}")
-    selection = input("Please select the index of desired CAM")
-    file_selected = unprocessed[int(selection)]
-    keys = list(gcp_folders.keys())
-    for key in keys:
-        if file_selected not in key:
-            gcp_folders.pop(key)
+#     #Generate list of all unprocessed cams
+#     unprocessed = list(set([name.split("/")[1] for name in unprocessed]))
+#     unprocessed = sorted([x for x in unprocessed if len(x) > 1])
+#     logger.warning("please select a cam to download")
+#     for idx, file in enumerate(unprocessed):
+#         logger.warning(f"{idx} : {file}")
+#     selection = input("Please select the index of desired CAM")
+#     file_selected = unprocessed[int(selection)]
+#     keys = list(gcp_folders.keys())
+#     for key in keys:
+#         if file_selected not in key:
+#             gcp_folders.pop(key)
     
-    input_path = save_path.replace("output" , "inputdata")
+#     input_path = save_path.replace("output" , "inputdata")
 
-    #TODO Add if secondary confirm if the cam has already been processed.  aka do folders exist and 
-    #Process each folder
-    for folder, files in gcp_folders.items():
-        # if its a results folder, skip it
-        if "results" in folder:
-            logger.info("Skipping results folder")
-            continue
-        # If its the unprocessed root folder skip
-        if folder == "unprocessed":
-            continue
-        #If there aren't any files in the folder, 
-        if not files:
-            logger.info(f"Skipping empty folder {folder}")
-            continue
+#     #Process each folder
+#     for folder, files in gcp_folders.items():
+#         # if its a results folder, skip it
+#         if "results" in folder:
+#             logger.info("Skipping results folder")
+#             continue
+#         # If its the unprocessed root folder skip
+#         if folder == "unprocessed":
+#             continue
+#         #If there aren't any files in the folder, 
+#         if not files:
+#             logger.info(f"Skipping empty folder {folder}")
+#             continue
 
-        logger.info(f"Processing folder {folder}")
-        #Process each file
-        for blob in files:
-            #Grab the name
-            cam_name = blob.name[blob.name.rindex("/")+1:]       
-            cam_f = cam_name.split(".")[0]
+#         logger.info(f"Processing folder {folder}")
+#         #Process each file
+#         for blob in files:
+#             #Grab the name
+#             cam_name = blob.name[blob.name.rindex("/")+1:]       
+#             cam_f = cam_name.split(".")[0]
 
-            #Generate filename save path
-            input_file_p = "/".join([input_path, cam_f, cam_name])
-            input_fold = os.path.join(input_path + "/" + cam_f + "/")
-            output_fold = os.path.join(save_path + "/" + cam_f + "/")
+#             #Generate filename save path
+#             input_file_p = "/".join([input_path, cam_f, cam_name])
+#             input_fold = os.path.join(input_path + "/" + cam_f + "/")
+#             output_fold = os.path.join(save_path + "/" + cam_f + "/")
 
-            #Check if input folder exists.  Create if not
-            if os.path.exists(input_fold):
-                logger.info(f"{cam_name} Input folder already exists")
-            else:
-                os.mkdir(input_fold)
-                logger.info(f"folder created @ {input_fold} ")
+#             #Check if input folder exists.  Create if not
+#             if os.path.exists(input_fold):
+#                 logger.info(f"{cam_name} Input folder already exists")
+#             else:
+#                 os.mkdir(input_fold)
+#                 logger.info(f"folder created @ {input_fold} ")
 
-            #Check to see if output folder exists. Create if not
-            if os.path.exists(output_fold):
-                logger.info(f"{cam_name} input folder already exists")
-            else:
-                os.mkdir(output_fold)
-                logger.info(f"folder created @ {output_fold} ")
+#             #Check to see if output folder exists. Create if not
+#             if os.path.exists(output_fold):
+#                 logger.info(f"{cam_name} input folder already exists")
+#             else:
+#                 os.mkdir(output_fold)
+#                 logger.info(f"folder created @ {output_fold} ")
 
-            #Check to see if input file exists. Download if not
-            # source_f = "/".join([configs["data_path"], cam_f, cam_name])
-            if os.path.exists(input_file_p):
-                logger.info(f"file {input_file_p} exists already")
-            else:
-                logger.info(f"Downloading {blob.name} to\n{input_file_p}")
-                blob.download_to_filename(input_file_p)
-                logger.info("ECG download complete.")
+#             #Check to see if input file exists. Download if not
+#             # source_f = "/".join([configs["data_path"], cam_f, cam_name])
+#             if os.path.exists(input_file_p):
+#                 logger.info(f"file {input_file_p} exists already")
+#             else:
+#                 logger.info(f"Downloading {blob.name} to\n{input_file_p}")
+#                 blob.download_to_filename(input_file_p)
+#                 logger.info("ECG download complete.")
             
-            if blob.name.endswith(".hea"):
-                file_names.append(input_file_p)
+#             if blob.name.endswith(".hea"):
+#                 file_names.append(input_file_p)
             
 
-    return file_names
+#     return file_names
 
-#FUNCTION Choose CAM
-def choose_cam(configs:dict, logger:logging)->list:
-    #BUG DELETE this func
-    """Choosing your cam to evaluate.  Logic is as follows.  If the inputdata directory on the install / or git clone is empty.  
-    Check the configs path setting.  If that value is None, exit program as no data can be found.
-
-    Args:
-        logger (logging): Logger
-
-    Raises:
-        ValueError: If neither directory has any header files, exit the program. 
-
-    Returns:
-        head_files (list): Returns a list 
-    """
-    #Check if the inputdata directory, load config_dir, and GCP flag
-    empty_inputdir = not os.listdir("./src/rad_ecg/data/inputdata")
-    config_dir = configs["data_path"]
-    gcp = configs["gcp_bucket"]
-
-    #If data is in the local inputdata folder with no call to GCP
-    if not empty_inputdir and not gcp:
-        head_files = get_records('inputdata')
-    #If there is a config directory target and gcp bucket is true
-    elif gcp:
-        head_files = download_ecg_from_gcs(configs["bucket_name"], configs["save_path"], logger)
-    # If the config directory target is valid
-    elif config_dir:
-        head_files = get_records(config_dir)
-    else:
-        logger.critical("No data found in inputdata dir or configs data_path")
-        exit()
-        
-    #Inquire which file you'd like to run
-    logger.warning("Please select the index of the CAM you would like to import.\nie: 1, 2, 3...")
-    if not gcp:
-        header_chosen = input("Please choose a CAM")
-    else:
-        header_chosen = "0"
-
-    if not header_chosen.isnumeric():
-        logger.critical(f'Incorrect file entered, program terminating')
-        exit()
-    elif int(header_chosen) not in range(len(head_files)):
-        logger.critical(f'File not in selection range.  program terminating')
-        exit()
-
-    else:
-        header_chosen = int(header_chosen)
-        head = head_files[header_chosen]
-        name = head.split(".")[-2].split("/")[-1]
-        if name in configs["save_path"]:
-            logger.warning(f'CAM {name} chosen')
-        else:
-            logger.critical(f"CAM {name} file not in output dir")
-            exit()
-
-    return head_files, header_chosen
-
-#FUNCTION Load Header Files
-def get_records(folder:str)->list:
-    #BUG - DELETE this fun
-    """Pulls the file info out of the data directory for file paths
-
-    Args:
-        p (str): [path to root data directory]
-
-    Returns:
-        dat_files (list): [List of dat names]
-        mib_files (list): [List of mib names]
-        head_files (list): [List of header names]
-    """
-
-    # There are 3 files for each record
-    #.dat = ECG data
-    #.hea = header file (file info)
-    #.mib = annotation file (beat annotations)
-    
-    #Get base directory
-    # p = os.path.normpath(os.getcwd() + os.sep + os.pardir)
-
-    #First check that the path isn't the standard inputdata directory
-    if folder != "inputdata":
-        base_dir = folder
-    else:
-        p = os.getcwd()
-        base_dir = os.path.join(p, "src" , "rad_ecg", "data", folder)
-    
-    dat_files, mib_files, head_files = [], [], []
-    for subject in os.scandir(base_dir):
-        if subject.is_dir():
-            for file_idx in os.scandir(subject.path):
-                if file_idx.name.endswith('.dat') and file_idx.is_file():
-                    dat_files.append(file_idx.path)
-
-                elif file_idx.name.endswith('.mib') and file_idx.is_file():
-                    mib_files.append(file_idx.path)
-
-                elif file_idx.name.endswith('.hea') and file_idx.is_file():
-                    head_files.append(file_idx.path)
-    
-    #leaving these here in case needed later #dat_files, mib_files
-    return head_files
 
 #FUNCTION Load Chart Data
 def load_chartdata(configs:dict, datafile:Path, logger:logging):
@@ -388,7 +286,6 @@ def launch_tui(configs:dict):
             guide_style="bold bright_blue",
         )
         files = walk_directory(Path(directory), tree)
-        #?Try a console print here too
         print(tree)
         
     question ="What file would you like to load?\n"
