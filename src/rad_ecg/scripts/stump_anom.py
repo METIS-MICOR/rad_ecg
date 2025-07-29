d42b2dbf9f52b68c2137796d70de2ffa953a7c9c
landcruiser87 <andyheroy@gmail.com>
2025-07-29 16:12:07 -0400

    updates

--- src/rad_ecg/scripts/stump_anom.py
+++ src/rad_ecg/scripts/stump_anom.py
@@ -1,74 +1,104 @@
-import numpy as np
 import stumpy
-from collections import deque
-from stumpy import gpu_stump
+import support
+import numpy as np
 import setup_globals
 from numba import cuda
-import support
-from support import logger
+from collections import deque
+from support import logger, console
 
-def run_stumpy_discord(ecg_data:dict, wave:np.array):
-    if cuda.is_available():
-        logger.debug("Algorithm running on GPU")
-        all_gpu_devices = [device.id for device in cuda.list_devices()]
-        if len(all_gpu_devices) > 1:
-            device_id = all_gpu_devices
+def group_numbers(arr, delta:int=10):
+    if not isinstance(arr, np.ndarray):
+        arr = np.array(arr)
+
+    if arr.size == 0:
+        return []
+
+    # Sort the array to make grouping easier
+    sorted_arr = np.sort(arr)
+    groups = []
+    current_group = []
+
+    for num in sorted_arr:
+        if not current_group:
+            current_group.append(num)
         else:
-            device_id = 0
-        stump_func = stumpy.gpu_stump
-    else:
-        logger.debug("Algorithm running on CPU")
-        device_id = None
-        stump_func = stumpy.stump
-    
-    sect_que = deque(ecg_data['section_info'][['start_point', 'end_point']])
-    sect_track = 0
-    
-    while len(sect_que) > 0:
-        section = sect_que.popleft()
-        start_p = section[0]
-        end_p = section[1]
-        sect_valid = ecg_data["section_info"]["valid"][sect_track]
-        match_delta = 5
-        match_count = 0
-        #Only compare valid sections. No R peak reference for failed area's.
-        if sect_valid:
-            peak_info = ecg_data['interior_peaks'][(ecg_data['interior_peaks'][:, 2] >= start_p) & (ecg_data['interior_peaks'][:, 2] <= end_p), :]
-            valid_QRS = peak_info[:, 5]
-            #NOTE maybe change to mean
-            m = round(np.median(np.diff(peak_info[:, 2])))
-            try:
-                TA = wave[start_p:end_p].flatten()
-                if device_id is not None:
-                    mp = stump_func(
-                        T_A = TA,
-                        m = m, 
-                        device_id = device_id
-                    )
-                else:
-                    mp = stump_func(
-                        T_A = TA,
-                        m = m,
-                    )
-            
-            except Exception as e:
-                logger.info(f'stumpy extraction error in section {sect_track}\n{e}')
-                mp = None
+            if abs(num - current_group[-1]) <= delta:
+                current_group.append(num)
+            else:
+                groups.append(current_group)
+                current_group = [num]
 
-            if mp is not None:
-                #Find the peaks that were invalid
-                invalid_r_peaks = peak_info[np.where(valid_QRS == 0)[0], 2]
+    if current_group:
+        groups.append(current_group)
 
-                #Perform scipy peak search again on the MP?
-                discord_idx = np.argsort(mp[:, 0])[:len(invalid_r_peaks)] + start_p
+    return [round(np.median(x)) for x in groups]
 
-                for discord in discord_idx:
-                    discord_range = range(discord - match_delta, discord + match_delta)
-                    if any(invalid_r_peaks) in discord_range:
-                        match_count += 1
+def run_stumpy_discord(ecg_data:dict, wave:np.array):    
+    progbar, job_id = support.mainspinner(console, len(sect_que))
+    with progbar:
+        if cuda.is_available():
+            logger.debug("Algorithm running on GPU")
+            all_gpu_devices = [device.id for device in cuda.list_devices()]
+            if len(all_gpu_devices) > 1:
+                device_id = all_gpu_devices
+            else:
+                device_id = 0
+            stump_func = stumpy.gpu_stump
+        else:
+            logger.debug("Algorithm running on CPU")
+            device_id = None
+            stump_func = stumpy.stump
         
-        sect_track += 1
+        sect_que = deque(ecg_data['section_info'][['start_point', 'end_point']])
+        sect_track = 0
+        
+        while len(sect_que) > 0:
+            progbar.update(task_id=job_id, description=f"[green] Stumpy Anomalies", advance=1)
+            section = sect_que.popleft()
+            start_p = section[0]
+            end_p = section[1]
+            sect_valid = ecg_data["section_info"]["valid"][sect_track]
+            match_delta = 10
+            match_count = 0
+            #Only compare valid sections. No R peak reference for failed area's.
+            if sect_valid:
+                peak_info = ecg_data['interior_peaks'][(ecg_data['interior_peaks'][:, 2] >= start_p) & (ecg_data['interior_peaks'][:, 2] <= end_p), :]
+                valid_QRS = peak_info[:, 5]
+                #NOTE maybe change to mean
+                m = round(np.median(np.diff(peak_info[:, 2])))
+                try:
+                    TA = wave[start_p:end_p].flatten()
+                    if device_id is not None:
+                        mp = stump_func(
+                            T_A = TA,
+                            m = m, 
+                            device_id = device_id
+                        )
+                    else:
+                        mp = stump_func(
+                            T_A = TA,
+                            m = m,
+                        )
+                
+                except Exception as e:
+                    logger.info(f'stumpy extraction error in section {sect_track}\n{e}')
+                    mp = None
 
+                if mp is not None:
+                    #Find the peaks that were invalid
+                    invalid_r_peaks = peak_info[np.where(valid_QRS == 0)[0], 2]
+
+                    #Perform scipy peak search again on the MP?
+                    discord_idx = np.argsort(mp[:, 0])[:len(invalid_r_peaks)] + start_p
+                    if discord_idx.shape[0] > 0:
+                        discords = group_numbers(discord_idx, match_delta)
+                        for discord in discords:
+                            discord_range = range(discord - match_delta, discord + match_delta)
+                            if any(invalid_r_peaks) in discord_range:
+                                match_count += 1
+
+            sect_track += 1
+        return match_count
 def main():
     global configs
     configs = setup_globals.load_config()
@@ -94,6 +124,7 @@ def main():
         if fname.endswith("_section_info.csv"):
             fpath = datafile._str + "\\" + fname.split("_section_info")[0]
             break
+    
     # NOTE: #Consider storing the mp results in here. 
     global ecg_data
     ecg_data = {
@@ -101,9 +132,8 @@ def main():
         "section_info": np.genfromtxt(fpath+"_section_info.csv", delimiter=",", dtype=wave_sect_dtype),
         "interior_peaks": np.genfromtxt(fpath+"_interior_peaks.csv", delimiter=",", dtype=np.int32, usecols=(range(16)), filling_values=0)
     } 
-
-    run_stumpy_discord(ecg_data, wave)
-    
+    match = run_stumpy_discord(ecg_data, wave)
+    print(match)
 if __name__ == "__main__":
     main()
 
