321bb1b15812a600850718dc439088fd65eaaec9
landcruiser87 <andyheroy@gmail.com>
2026-05-08 16:22:31 -0500

    quick fix

--- src/rad_ecg/scripts/porcine/pig_extract.py
+++ src/rad_ecg/scripts/porcine/pig_extract.py
@@ -1285,8 +1285,8 @@ class PigRAD:
             modeltraining.show_results(modellist, sort_des=False)
 
             #Gridsearch models
-            # console.print("[green]launch gridsearch...[/]")
-            # modeltraining._grid_search("xgboost")
+            console.print("[green]launch gridsearch...[/]")
+            modeltraining._grid_search("xgboost")
             
             #Finzalize report
             modeltraining.finalize_report(f"src/rad_ecg/data/logs/{DATE_JSON}_term.html")
